34155d031ff05bb0327949ba782bc0e7ffa59c04
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-02-15 08:39:30 +0900

    Create CoffeeBeanCard

---
+++ src/presentations/components/CoffeeBeanCard.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+
+import { ICoffeeBean } from 'interfaces';
+
+// tslint:disable-next-line:function-name
+export function CoffeeBeanCard(props: { coffeeBean: ICoffeeBean }): JSX.Element {
+  const coffeeBean: ICoffeeBean = props.coffeeBean;
+
+  return (
+    <div className="CoffeeBeanCard">
+      <div className="CoffeeBeanCard--Content">
+        <a href={`/coffee-beans/${coffeeBean.id}`}>
+          <img src={coffeeBean.imageUrl} alt={coffeeBean.name} />
+          <h2>{coffeeBean.name}</h2>
+          <p>{coffeeBean.description}</p>
+        </a>
+      </div>
+    </div>
+  );
+}
