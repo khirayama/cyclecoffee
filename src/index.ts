bae491bfaf17033c626c40c860f70db255fbd619
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-02-17 21:49:38 +0900

    Implement auth, signup, signin and signout mock handlers

--- src/index.ts
+++ src/index.ts
@@ -22,18 +22,46 @@ import { homeHandler } from 'handlers/homeHandler';
 import { shopHandler } from 'handlers/shopHandler';
 import { ICoffeeBean, IPlan, IShop } from 'interfaces';
 
-// tslint:disable-next-line:no-any
-const compiledFunction: (options: { props: any }) => void = pug.compileFile(
-  path.resolve('dist', 'presentations', 'application', 'Layout.pug'),
-  {
-    basedir: path.resolve('dist', 'presentations'),
-  },
-);
-
 function preHandler(req: express.Request, res: express.Response, next: express.NextFunction): void {
-  req.compiledFunction = compiledFunction;
+  req.compiledFunction = pug.compileFile(path.resolve('dist', 'presentations', 'application', 'Layout.pug'), {
+    basedir: path.resolve('dist', 'presentations'),
+  });
   next();
 }
+
+function authMockHandler(req: express.Request, res: express.Response, next: express.Next): void {
+  req.isSignedIn = req.cookies.session === 'true';
+  next();
+}
+
+function signInMockHandler(req: express.Request, res: express.Response): void {
+  res.cookie('session', true);
+  res.redirect('/');
+}
+
+function signOutMockHandler(req: express.Request, res: express.Response): void {
+  res.clearCookie('session');
+  res.redirect('/');
+}
+
+const web: express.Router = express.Router();
+const api: express.Router = express.Router();
+
+web
+  .use(preHandler)
+  .use(authMockHandler)
+  .get('/', homeHandler)
+  .get('/signin', signInMockHandler)
+  .get('/signup', signInMockHandler)
+  .get('/signout', signOutMockHandler)
+  .get('/coffee-beans/:id', coffeeBeanHandler)
+  .get('/shops/:id', shopHandler);
+api
+  .get('/coffee-beans', coffeeBeansAPIHandler)
+  .get('/coffee-beans/:id', coffeeBeanAPIHandler)
+  .get('/shops', shopsAPIHandler)
+  .get('/shops/:id', shopAPIHandler)
+  .get('/plans', plansAPIHandler);
 
 const app: express = express();
 
@@ -44,26 +72,10 @@ app
   .use(express.static(path.join(__dirname, 'assets')))
   .use(express.static(path.join(__dirname, 'public')))
   .use(bodyParser.json())
-  .use(cookieParser())
-  .use(preHandler);
-
-const web: express.Router = express.Router();
-const api: express.Router = express.Router();
-
-web
-  .get('/', homeHandler)
-  .get('/coffee-beans/:id', coffeeBeanHandler)
-  .get('/shops/:id', shopHandler);
-api
-  .get('/coffee-beans', coffeeBeansAPIHandler)
-  .get('/coffee-beans/:id', coffeeBeanAPIHandler)
-  .get('/shops', shopsAPIHandler)
-  .get('/shops/:id', shopAPIHandler)
-  .get('/plans', plansAPIHandler);
+  .use(cookieParser());
 
 app.use(web).use('/api', api);
 
-// Server
 const APP_SERVER_PORT: number = Number(process.env.PORT || '3030');
 app.listen(APP_SERVER_PORT, () => {
   // tslint:disable-next-line:no-console
