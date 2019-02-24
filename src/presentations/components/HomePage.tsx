4aa75d197130a7fa5496764fccede1d7e373ed6a
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-02-25 07:38:34 +0900

    Update home page

--- src/presentations/components/HomePage.tsx
+++ src/presentations/components/HomePage.tsx
@@ -26,41 +26,32 @@ export class HomePage extends React.Component<IProps> {
         <Header isSignedIn={this.props.isSignedIn} />
         <div className="HomePage--Message">
           <div className="HomePage--Message--Content">
-            <h2>
-              素敵なコーヒーとの出会いを。
-              <br />
-              あなたの過ごし方のパートナーに。
-              <br />
-              コーヒー豆定期便、お届けします。
-            </h2>
+            <h2>なんとかかんとか、コーヒー便</h2>
+            <a href="/app">Start on WEB</a>
           </div>
         </div>
         <div className="HomePage--Steps">
           <div className="HomePage--Steps--Content">
-            <h2 className="HomePage--Steps--Content--Heading">cycle coffeeが届くまで</h2>
             <ul className="HomePage--Steps--Content--List">
               <li className="HomePage--Steps--Content--List--Item">
                 <div className="HomePage--Steps--Content--List--Item--Step">
-                  <h3>1. マイページを作ります。</h3>
+                  <h3>選べるコーヒー豆</h3>
                   <p>
-                    登録（サインアップ）して、マイページを作ります。
-                    マイページではお好きなコーヒー豆を選んだり、お届け先変更、スキップ、決済方法変更などが行えます。
+                    厳選されたコーヒーロースターのコーヒー豆をお届けします。
+                    産地、焙煎具合、ディカフェなど、さまざまなテイスト、シーンに合わせて選べます。
                   </p>
                 </div>
               </li>
               <li className="HomePage--Steps--Content--List--Item">
                 <div className="HomePage--Steps--Content--List--Item--Step">
-                  <h3>2. コーヒー豆を選びます。</h3>
-                  <p>
-                    マイページなどから次回飲んでみたいコーヒー豆を選びます。
-                    毎回自由に組み合わせることもできます。お気に入りのコーヒー豆を見つけてください！
-                  </p>
+                  <h3>ポストに投函</h3>
+                  <p>コーヒー豆は専用ボックスでポストにお届け。忙しくても受け取れます。</p>
                 </div>
               </li>
               <li className="HomePage--Steps--Content--List--Item">
                 <div className="HomePage--Steps--Content--List--Item--Step">
-                  <h3>3. ポストにお届け</h3>
-                  <p>お選びいただいたコーヒー豆をポストにお届けします。 月に2度、1日と15日に発送します。</p>
+                  <h3>美味しく飲める</h3>
+                  <p>ロースターが勧める最適なレシピもご紹介。より美味しく、よりよい時間を。</p>
                 </div>
               </li>
             </ul>
@@ -68,19 +59,17 @@ export class HomePage extends React.Component<IProps> {
         </div>
         <div className="HomePage--Plans">
           <div className="HomePage--Plans--Content">
-            <h2 className="HomePage--Plans--Content--Heading">
-              月に2度、新鮮なコーヒー豆をお届けます。お好みの量をお選びください。
-            </h2>
+            <h2 className="HomePage--Plans--Content--Heading">月に2度、新鮮なコーヒー豆をお届けます。</h2>
             <ul className="HomePage--Plans--Content--List">
               {plans.map((plan: IPlan) => {
                 return (
                   <li key={plan.id} className="HomePage--Plans--Content--List--Item">
                     <div className="HomePage--Plans--Content--List--Item--Plan">
-                      <h2>{plan.name}</h2>
                       <p>{plan.price} 円</p>
                       <p>{plan.numberOfShipping * plan.numberOfPack * plan.amountOfPack} g / 月</p>
                       <p>
-                        月 {plan.numberOfShipping} 回、 {plan.amountOfPack * plan.numberOfPack} gずつのお届け。
+                        隔週月{plan.numberOfShipping}回、 {plan.amountOfPack * plan.numberOfPack}{' '}
+                        gずつのお届け。新鮮なコーヒー豆をずっと。
                       </p>
                     </div>
                   </li>
@@ -89,9 +78,30 @@ export class HomePage extends React.Component<IProps> {
             </ul>
           </div>
         </div>
+        <div className="HomePage--Steps">
+          <div className="HomePage--Steps--Content">
+            <h2 className="HomePage--Steps--Content--Heading">どんなコーヒー豆があるの？</h2>
+            <ul className="HomePage--Steps--Content--List">
+              <li className="HomePage--Steps--Content--List--Item">
+                <div className="HomePage--Steps--Content--List--Item--Step">
+                  <h3>おいしいコーヒー豆だけ</h3>
+                  <p>厳選されたコーヒーロースターから自信を持っておすすめできるコーヒー豆をお届けします。</p>
+                </div>
+              </li>
+              <li className="HomePage--Steps--Content--List--Item">
+                <div className="HomePage--Steps--Content--List--Item--Step">
+                  <h3>出来立てのコーヒー豆を</h3>
+                  <p>
+                    ご注文確定日以降に焙煎したコーヒー豆をお届けします。(配送地域によって、より期間が開いてしまうことがあります)
+                  </p>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
         <div className="HomePage--CoffeeBeans">
           <div className="HomePage--CoffeeBeans--Content">
-            <h2 className="HomePage--CoffeeBeans--Content--Heading">コーヒー豆をお選びいただけます</h2>
+            <h2 className="HomePage--CoffeeBeans--Content--Heading">例えば...</h2>
             <ul className="HomePage--CoffeeBeans--Content--List">
               {coffeeBeans.map((coffeeBean: ICoffeeBean) => {
                 const shop: IShop = shops.filter((targetShop: IShop) => targetShop.id === coffeeBean.shopId)[0];
@@ -110,6 +120,35 @@ export class HomePage extends React.Component<IProps> {
               })}
             </ul>
           </div>
+        </div>
+        <div className="HomePage--Steps">
+          <div className="HomePage--Steps--Content">
+            <h2 className="HomePage--Steps--Content--Heading">楽しんで続けられる</h2>
+            <ul className="HomePage--Steps--Content--List">
+              <li className="HomePage--Steps--Content--List--Item">
+                <div className="HomePage--Steps--Content--List--Item--Step">
+                  <h3>そのまま保存</h3>
+                  <p>2週間の保存に耐えられるコンパクトなパッケージでお届け。</p>
+                </div>
+              </li>
+              <li className="HomePage--Steps--Content--List--Item">
+                <div className="HomePage--Steps--Content--List--Item--Step">
+                  <h3>webで管理</h3>
+                  <p>webアプリで、お届け予定のコーヒー豆や新規追加、スキップなど管理できます。</p>
+                </div>
+              </li>
+              <li className="HomePage--Steps--Content--List--Item">
+                <div className="HomePage--Steps--Content--List--Item--Step">
+                  <h3>安心の保証つき</h3>
+                  <p>万が一、状態がよくないコーヒー豆が届いても、新しいものを無料で再送します。</p>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div>
+          <p>なんとかかんとか、コーヒー便</p>
+          <a href="/app">Start on WEB</a>
         </div>
         <Footer />
       </div>
