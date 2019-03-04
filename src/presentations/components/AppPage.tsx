bc99d4c5627d4f8df6b60fe091cd7afae17bca13
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-03-04 09:43:42 +0900

    Update AppPage

--- src/presentations/components/AppPage.tsx
+++ src/presentations/components/AppPage.tsx
@@ -4,12 +4,15 @@ import { ICoffeeBean, IPlan, IShop } from 'interfaces';
 import { CoffeeBeanCard } from 'presentations/components/CoffeeBeanCard';
 import { Footer } from 'presentations/components/Footer';
 import { Header } from 'presentations/components/Header';
+import { CoffeeBeanSelectButtonContainer } from 'presentations/containers/CoffeeBeanSelectButtonContainer';
 
 export interface IProps {
   isSignedIn: boolean;
   plans: IPlan[];
   coffeeBeans: ICoffeeBean[];
   shops: IShop[];
+  isSkipped: boolean;
+  selectedCoffeeBeanIds: string[];
 }
 
 export class AppPage extends React.Component<IProps> {
@@ -17,27 +20,37 @@ export class AppPage extends React.Component<IProps> {
     const coffeeBeans: ICoffeeBean[] = this.props.coffeeBeans;
     const plans: IPlan[] = this.props.plans;
     const shops: IShop[] = this.props.shops;
+    const isSkipped: boolean = this.props.isSkipped;
+    const selectedCoffeeBeanIds: string[] = this.props.selectedCoffeeBeanIds;
 
     return (
       <div className="AppPage">
         <Header isSignedIn={this.props.isSignedIn} />
         <div className="AppPage--Content">
-          <div>最初のコーヒー豆を選ぼう</div>
-          <ul className="AppPage--Content--CoffeeBeanList">
-            {coffeeBeans.map((coffeeBean: ICoffeeBean) => {
-              return (
-                <li key={coffeeBean.id} className="AppPage--Content--CoffeeBeanList--Item">
-                  <CoffeeBeanCard coffeeBean={coffeeBean} />
-                </li>
-              );
-            })}
-          </ul>
-          <div>次回お届け予定のコーヒー豆</div>
-          <CoffeeBeanCard coffeeBean={coffeeBeans[0]} />
-          <small>生産状況により代理豆をお届けする場合があります。ご了承ください。</small>
+          {selectedCoffeeBeanIds.length ? (
+            <>
+              <div>次回お届け予定のコーヒー豆</div>
+              <CoffeeBeanCard coffeeBean={coffeeBeans[0]} />
+              <small>生産状況により代理豆をお届けする場合があります。ご了承ください。</small>
+            </>
+          ) : (
+            <>
+              <div>最初のコーヒー豆を選ぼう</div>
+              <ul className="AppPage--Content--CoffeeBeanList">
+                {coffeeBeans.map((coffeeBean: ICoffeeBean) => {
+                  return (
+                    <li key={coffeeBean.id} className="AppPage--Content--CoffeeBeanList--Item">
+                      <CoffeeBeanCard coffeeBean={coffeeBean} />
+                      <CoffeeBeanSelectButtonContainer />
+                    </li>
+                  );
+                })}
+              </ul>
+            </>
+          )}
           <div>ロースター紹介</div>
           <div>スキップ</div>
-          <input type="checkbox" checked={false} onChange={() => console.log('change')} />
+          <input type="checkbox" checked={isSkipped} onChange={() => console.log('change')} />
           <div />
         </div>
         <Footer />
