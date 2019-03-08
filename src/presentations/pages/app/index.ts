1643d4046f20b237ed13029533167b26399191a5
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-03-08 20:33:45 +0900

    Update app page

--- src/presentations/pages/app/index.ts
+++ src/presentations/pages/app/index.ts
@@ -1,9 +1,16 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
+import { IAction } from 'interfaces';
 import { AppPage } from 'presentations/components/AppPage';
+import { Provider } from 'utils/Container';
+import { Store } from 'utils/Store';
+import { reducer } from 'presentations/pages/app/reducer';
+import { IState } from 'presentations/pages/app/interfaces';
+
+const store: Store<IState, IAction> = new Store(window.state, reducer);
 
 window.addEventListener('DOMContentLoaded', () => {
   const el: HTMLElement = window.document.querySelector('.application');
-  ReactDOM.render(React.createElement(AppPage, window.state), el);
+  ReactDOM.render(React.createElement(Provider, { store }, React.createElement(AppPage, window.state)), el);
 });
